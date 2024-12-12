bf590f4d4169cd7fd0d81af11f2894a8094df45e
Alisson2003 <alisson.viracocha@epn.edu.ec>
2024-12-11 23:49:42 -0500

    Initial commit

---
+++ src/form1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class form1 {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+}
